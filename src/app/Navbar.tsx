7a50312ebee434be179490fe02cf9b5c06162a2e
yahav3801 <ybh00790@gmail.com>
2025-01-23 16:02:19 +0200

    member login oauth callback

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,13 +1,19 @@
-import { getWixClient } from "@/lib/wix-client.base";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
 import Image from "next/image";
 import { getCart } from "@/wix-api/cart";
 import { getWixServerClient } from "@/lib/wix-client.server";
 import ShoppingCartButton from "./ShoppingCartButton";
+import UserButton from "@/components/UserButton";
+import { getLoggedInMember } from "@/wix-api/members";
 
 export default async function Navbar() {
-  const cart = await getCart(await getWixServerClient());
+  const wixClient = getWixServerClient();
+
+  const [cart, loggedInMember] = await Promise.all([
+    getCart(await wixClient),
+    getLoggedInMember(await wixClient),
+  ]);
 
   return (
     <header className="bg-background shadow-sm">
@@ -16,7 +22,10 @@ export default async function Navbar() {
           <Image src={logo} alt="logo" width={40} height={40} />
           <span className="text-xl font-bold">Flow Shop</span>
         </Link>
-        <ShoppingCartButton initialData={cart} />
+        <div className="flex items-center justify-center gap-5">
+          <UserButton loggedInMember={loggedInMember} />
+          <ShoppingCartButton initialData={cart} />
+        </div>
       </div>
     </header>
   );
---
+++ src/components/UserButton.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import useAuth from "@/hooks/auth";
+import { members } from "@wix/members";
+import {
+  Check,
+  LogInIcon,
+  LogOutIcon,
+  Monitor,
+  Moon,
+  Sun,
+  UserIcon,
+} from "lucide-react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+
+interface UserButtonProps {
+  loggedInMember: members.Member | null;
+  className?: string;
+}
+
+export default function UserButton({
+  loggedInMember,
+  className,
+}: UserButtonProps) {
+  const { login, logout } = useAuth();
+
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="icon" variant="ghost" className={className}>
+          <UserIcon className="scale-150" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="min-w-44 max-w-64">
+        {loggedInMember && (
+          <>
+            <DropdownMenuLabel>
+              Logged in as{" "}
+              {loggedInMember.contact?.firstName || loggedInMember.loginEmail}
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <Link href="/profile">
+              <DropdownMenuItem>
+                <UserIcon className="mr-2 size-4" />
+                Profile
+              </DropdownMenuItem>
+            </Link>
+            <DropdownMenuSeparator />
+          </>
+        )}
+
+        {loggedInMember ? (
+          <DropdownMenuItem onClick={() => logout()}>
+            <LogOutIcon className="mr-2 size-4" />
+            Logout
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => login()}>
+            <LogInIcon className="mr-2 size-4" />
+            Login
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
