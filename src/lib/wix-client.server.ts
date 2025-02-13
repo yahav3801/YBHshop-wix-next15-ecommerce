d9b559c7816d15e1e41b91813dc90fa671237645
yahav3801 <ybh00790@gmail.com>
2025-02-13 12:14:09 +0200

    admin apikey & media upload path done

--- src/lib/wix-client.server.ts
+++ src/lib/wix-client.server.ts
@@ -1,8 +1,10 @@
-import { Tokens } from "@wix/sdk";
+import { ApiKeyStrategy, createClient, Tokens } from "@wix/sdk";
 import { getWixClient } from "./wix-client.base";
 import { cookies } from "next/headers";
 import { WIX_SESSION_COOKIE } from "./constants";
-
+import { cache } from "react";
+import { files } from "@wix/media";
+import { env } from "@/env";
 export async function getWixServerClient() {
   let tokens: Tokens | undefined;
   try {
@@ -12,3 +14,16 @@ export async function getWixServerClient() {
   } catch (error) {}
   return getWixClient(tokens);
 }
+
+export const getWixAdminClient = cache(() => {
+  const wixClient = createClient({
+    modules: {
+      files,
+    },
+    auth: ApiKeyStrategy({
+      apiKey: env.WIX_API_KEY,
+      siteId: env.NEXT_PUBLIC_WIX_SITE_ID,
+    }),
+  });
+  return wixClient;
+});
