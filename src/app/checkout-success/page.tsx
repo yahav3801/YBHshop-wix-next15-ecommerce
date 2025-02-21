52196effd079d45285ac1bd781854ce4559d1a45
yahav3801 <ybh00790@gmail.com>
2025-02-21 14:46:59 +0200

    try again

--- src/app/checkout-success/page.tsx
+++ src/app/checkout-success/page.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default async function Page({
   searchParams,
 }: {
-  searchParams: { orderId?: string }; // Make orderId optional
+  searchParams: { orderId: string };
 }) {
   const { orderId } = searchParams;
   if (!orderId) notFound();
